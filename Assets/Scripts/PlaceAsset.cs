dc01cd17040c53174b0f3e72f9155be54b15023d
Anne-Sophie Koch <anne.sophie.koch54@gmail.com>
2022-01-24 12:12:39 +0100

    show assets

--- Assets/Scripts/PlaceAsset.cs
+++ Assets/Scripts/PlaceAsset.cs
@@ -11,16 +11,11 @@ using Emgu.CV.Util;
 
 public class PlaceAsset : MonoBehaviour
 {
-    public GameObject orc;
     public List<GameObject> listObject;
     public Material terrainMaterial;
+    private Dictionary<int, GameObject> alreadyInPlace = new Dictionary<int, GameObject>();
 
     Dictionary dictMarkers = new Dictionary(Dictionary.PredefinedDictionaryName.Dict6X6_250);
-
-    public void displayAsset(VectorOfPointF pointMarqueur, int marqueurID)
-    {
-       
-    }
 
     public void createTerrain(Mat rotvectors, Mat transvectors, int markerID)
     {
@@ -37,9 +32,11 @@ public class PlaceAsset : MonoBehaviour
         else
         {
             TerrainObj = new GameObject("TerrainObj");
-        }        
+        }
 
         TerrainData _TerrainData = new TerrainData();
+
+        Debug.Log(_TerrainData);
 
         _TerrainData.size = new Vector3(10, 600, 10);
         _TerrainData.heightmapResolution = 512;
@@ -52,7 +49,10 @@ public class PlaceAsset : MonoBehaviour
         TerrainCollider _TerrainCollider = TerrainObj.AddComponent<TerrainCollider>();
         Terrain _Terrain2 = TerrainObj.AddComponent<Terrain>();
 
-        _TerrainCollider.terrainData = _TerrainData;
+        Debug.Log(_TerrainCollider);
+        Debug.Log(_Terrain2);
+
+        //_TerrainCollider.terrainData = _TerrainData;
         _Terrain2.terrainData = _TerrainData;
         _Terrain2.materialTemplate = terrainMaterial;
 
@@ -62,7 +62,9 @@ public class PlaceAsset : MonoBehaviour
         //GameObject _Terrain = Terrain.CreateTerrainGameObject(_TerrainData);
 
     }
-    public void displayAsset(Mat rotvectors, Mat transvectors, int marqueurID)
+
+
+    public void displayAssetInWorldAR(Mat rotvectors, Mat transvectors, int marqueurID)
     {
         (Vector3, Quaternion) transformAsset = computeTransform(rotvectors, transvectors);
 
@@ -84,9 +86,56 @@ public class PlaceAsset : MonoBehaviour
 
         Vector3 worldPos = transform.TransformPoint(localPos);
 
+        
         double flip = (double)rotvectors.GetData().GetValue(0, 0, 0);
         flip = -flip;
         rotvectors.GetData().SetValue(flip, 0, 0, 0);
+        
+
+        Mat rotMatrix = new Mat();
+        CvInvoke.Rodrigues(rotvectors, rotMatrix);
+
+        
+        Vector3 forward;
+        forward.x = float.Parse(rotMatrix.GetData().GetValue(2, 0).ToString());
+        forward.y = float.Parse(rotMatrix.GetData().GetValue(2, 1).ToString());
+        forward.z = float.Parse(rotMatrix.GetData().GetValue(2, 2).ToString());
+
+        Vector3 up;
+        up.x = float.Parse(rotMatrix.GetData().GetValue(1, 0).ToString());
+        up.y = float.Parse(rotMatrix.GetData().GetValue(1, 1).ToString());
+        up.z = float.Parse(rotMatrix.GetData().GetValue(1, 2).ToString());
+
+        forward.Normalize();
+        up.Normalize();
+        
+
+        Quaternion rot = Quaternion.LookRotation(forward, up);
+
+        rot *= Quaternion.Euler(0, 0, 180);
+
+        Quaternion worldRot = transform.rotation * rot;
+
+        return (worldPos, worldRot);
+    }
+
+    public void displayAsset(Mat rotvectors, Mat transvectors, int marqueurID)
+    {
+
+        Debug.Log(float.Parse(transvectors.GetData().GetValue(0, 0, 0).ToString()));
+
+        Vector3 localPos;
+        localPos.x = float.Parse(transvectors.GetData().GetValue(0, 0, 0).ToString());
+        localPos.y = -float.Parse(transvectors.GetData().GetValue(0, 0, 1).ToString());
+        localPos.z = float.Parse(transvectors.GetData().GetValue(0, 0, 2).ToString());
+
+        //Vector3 worldPos = transform.TransformPoint(localPos);
+
+        
+        double flip = (double)rotvectors.GetData().GetValue(0, 0, 0);
+        flip = -flip;
+        rotvectors.GetData().SetValue(flip, 0, 0, 0);
+        
 
         Mat rotMatrix = new Mat();
         CvInvoke.Rodrigues(rotvectors, rotMatrix);
@@ -101,15 +150,24 @@ public class PlaceAsset : MonoBehaviour
         up.y = float.Parse(rotMatrix.GetData().GetValue(1, 1).ToString());
         up.z = float.Parse(rotMatrix.GetData().GetValue(1, 2).ToString());
 
-        forward.Normalize();
-        up.Normalize();
-
         Quaternion rot = Quaternion.LookRotation(forward, up);
 
         rot *= Quaternion.Euler(0, 0, 180);
 
-        Quaternion worldRot = transform.rotation * rot;
+        //Quaternion worldrot = transform.rotation * rot;
 
-        return (worldPos, worldRot);
+        if (alreadyInPlace.ContainsKey(marqueurID))
+        {
+            //Debug.Log("meh");
+            alreadyInPlace[marqueurID].transform.position = localPos;
+            alreadyInPlace[marqueurID].transform.rotation = rot;
+        }
+        else
+        {
+            GameObject obj = listObject[marqueurID];
+            obj.transform.position = localPos;
+            obj.transform.rotation = rot;
+            alreadyInPlace.Add(marqueurID, Instantiate(obj));
+        }
     }
 }
